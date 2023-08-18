749a8f6ed1ac2f9b7884d4902e938ac6bf38d479
Carlo Pascoli <carlo.pascoli@gmail.com>
2023-08-18 09:00:13 +0100

    Add contract-sizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,7 @@ import "hardhat-contract-sizer";
 require('dotenv').config()
 
 
-const { RPC_URL_POLYGON_MAIN, RPC_URL_GOERLI, ETHERSCAN_API_KEY, POLYGONSCAN_API_KEY, MNEMONIC } = process.env;
+const { RPC_URL_POLYGON_MAIN, RPC_URL_GOERLI, ETHERSCAN_API_KEY, POLYGONSCAN_API_KEY, MNEMONIC, DEPLOYER_PRIVATE_KEY } = process.env;
 
 const config : HardhatUserConfig = {
   
@@ -30,6 +30,10 @@ const config : HardhatUserConfig = {
     artifacts: './artifacts',
   },
 
+  mocha: {
+    timeout: 60000
+  },
+
   abiExporter: {
     path: './build/abi',
     clear: true,
@@ -46,21 +50,32 @@ const config : HardhatUserConfig = {
     gasPrice: 200
   },
 
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: true,
+    only: [':PoolV5$'],
+  },
+
   networks: {
     hardhat: {
       forking: {
-        url: RPC_URL_POLYGON_MAIN || "",
-        blockNumber: 42001664  // 38702537 
-      }
+        url: RPC_URL_POLYGON_MAIN || ",",
+        blockNumber: 42817100  // 42449622// post V4 deploymet: 42449622 // 42001664 // 42293851      // hack start block: 42001665  - hack end block: 42003217 ,
+      },
+      accounts: {accountsBalance: "10000000000000000000000"}
     },
     goerli: {
       url: RPC_URL_GOERLI,
-      accounts: { mnemonic: MNEMONIC  },
+      // accounts: { mnemonic: MNEMONIC  },
+      accounts: [DEPLOYER_PRIVATE_KEY ?? ""],
     },
     polygon: {
       url: RPC_URL_POLYGON_MAIN,
-      accounts: { mnemonic: MNEMONIC  },
-      gasPrice:  120000000000,  // 120 Gwei
+      // accounts: { mnemonic: MNEMONIC  },
+      accounts: [DEPLOYER_PRIVATE_KEY ?? ""],
+      gasPrice:  180000000000,  // 250 Gwei
     },
   },
 
